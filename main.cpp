1eccccedd8ba9da6fedfff45a2a82f6f04241d8c
Lois Diserens <lois.diserens@hispeed.ch>
2020-03-27 17:05:50 +0100

    Premier push

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : <nn>
+ Fichier     : <nom du fichier>.cpp
+ Auteur(s)   : <prénom> <nom>
+ Date        : <jj.mm.aaaa>
+
+ But         : <à compléter>
+
+ Remarque(s) : <à compléter>
+
+ Compilateur : MinGW-g++ 6.3.0
+ -----------------------------------------------------------------------------------
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+
+int main() 
+{
+   
+   
+   system("PAUSE");
+   return EXIT_SUCCESS;
+}
